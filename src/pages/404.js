cb0aecf587ebebd054cc7d05bd7bc19332294480
balogun lukman <lukheebalo@gmail.com>
2021-07-25 00:25:07 +0100

    chore: 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,6 @@
 import * as React from "react"
+import { navigate } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -6,8 +8,21 @@ import Seo from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="container bg-light text-center my-4">
+      <div className="col col-md-6text-center m-auto">
+        <StaticImage
+          src="../images/page-not-found.svg"
+          alt="logo 2"
+          placeholder="blurred"
+          width={200}
+          className="m-auto mb-3"
+        />
+      </div>
+      <p className="text-danger"> Page you are viewing doesn't exit please</p>
+      <button className="btn btn-secondary" onClick={() => navigate("/")}>
+        Go Back Home
+      </button>
+    </div>
   </Layout>
 )
 
